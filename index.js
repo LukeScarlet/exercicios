d8b62d84da389f741cf78765ac0a7f5d198e30e5
LUCA FLORES BONFARDINI <05319727046@senacrs.edu.br>
2024-03-05 11:38:39 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//comando para exibir mensagens
+console.log("O INTER VAI SAIR CAMPEAO E O GREMIO NAO!!!")
+
+//exercício de cálculos
+
+const num1 = 20;
+const num2 = 654;
+
+//soma
+
+let soma1 = num1 + num2
+let soma2 = num1 - num2
+let soma3 = num1 * num2
+let soma4 = num1 / num2
+
+console.log("O valor da some é " + soma1 + " - daleeeeeeee inter");
+console.log("O valor da subtração é " + soma2)
+console.log("O valor da multiplicação é " + soma3)
+console.log("O valor da divisão é " + soma4)
